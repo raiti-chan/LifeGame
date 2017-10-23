a6221212ead282f9db57a0f9709120e230859c97
raiti-chan <rusuke@t-net.ne.jp>
2017-10-23 10:31:15 +0900

    Initial commit

---
+++ LifeGameC8x8/lifegame_c8x8.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <random>
+#include <bitset>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+
+
+typedef unsigned long long ulong;
+
+ulong nextGeneration(ulong bord);
+
+ulong randULong();
+
+void outBord(ulong bord);
+
+std::mt19937_64 *mt;
